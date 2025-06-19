4a21e88720522bc29fd608abdc81325563299b88
Subham Das <subhamd3005@gmail.com>
2025-06-19 14:14:31 +0530

    Add files via upload
    Variables

---
+++ 2-script.js
@@ -0,0 +1,38 @@
+//let x; //declaration
+//x = 100;//assignment
+
+/*letx = 100; declaration + assignment
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+let firstName ="Bro";
+let favouriteFood = "Pizza";
+let email = "Bro@gmail.com";
+
+/*console.log(x);
+
+console.log(age);
+console.log(price);
+console.log(`You are ${age} years old`);
+console.log(`The price is ${price}`);
+console.log(`Your gpa is ${gpa}`);
+
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`You like ${favouriteFood}`);
+console.log(`Your email is ${email}`); 
+
+let online = true;
+let forSale = true;
+console.log(`Bro is online: ${online}`);
+console.log(`Is this car for sale ${forSale}`);*/
+
+let fullName = "Bro Code";
+let age = 20;
+let isStudent = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
