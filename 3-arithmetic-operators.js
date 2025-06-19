a6c2ad85594bee3687fe43d79410678f458c9c56
Subham Das <subhamd3005@gmail.com>
2025-06-19 17:57:39 +0530

    Add files via upload

---
+++ 3-arithmetic-operators.js
@@ -0,0 +1,31 @@
+let students = 30;
+
+//students =students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2; //exponential operator
+//let extrastudents = students % 3; // remainder or modulus operator
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+
+//students **=2;
+//students %= 2;
+
+//students++;
+//students--;
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication division and modulo
+    4.addition and substraction
+    */
+
+//console.log(students);
+
+let result = 1+2*3+4**2;
+console.log(result);
