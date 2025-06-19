d25a0cbcca1a97653038e01707e3f85782e8e368
Subham Das <subhamd3005@gmail.com>
2025-06-19 18:00:16 +0530

    Add files via upload

---
+++ 5-Type-onversion.js
@@ -0,0 +1,19 @@
+/* type-conversion=chnage the datatype of a value to another 
+                    (Strings ,numbers , boolean)*/
+/*let age = window.prompt("How old are you?");
+age = Number(age);
+age+=1;
+
+console.log(age, typeof age);*/
+
+let x = "";
+let y = "";
+let z = "";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
