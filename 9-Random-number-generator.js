79feec3d70f660eba3fe92d32db750a57c409822
Subham Das <subhamd3005@gmail.com>
2025-06-20 12:14:56 +0530

    9-RandomNumberGenerator/

---
+++ 9-Random-number-generator.js
@@ -0,0 +1,27 @@
+//random Number generator
+
+/*const min = 50;
+const max = 100;
+
+let randomNum = Math.floor(Math.random() * (max - min)) + min;
+
+console.log(randomNum);*/
+
+const myButton= document.getElementById("myButton");
+const myLabel1 = document.getElementById("myLabel1");
+const myLabel2 = document.getElementById("myLabel2");
+const myLabel3 = document.getElementById("myLabel3");
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function(){
+let randomNum1 = Math.floor(Math.random() * max) + min;
+let randomNum2 = Math.floor(Math.random() * max) + min;
+let randomNum3 = Math.floor(Math.random() * max) + min;
+myLabel1.textContent = randomNum1;
+myLabel2.textContent = randomNum2;
+myLabel3.textContent = randomNum3;
+}
