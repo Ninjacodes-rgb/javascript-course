8b0a04e6ac60e4a436a91ebcdfa17789f8791142
Subham Das <subhamd3005@gmail.com>
2025-06-30 11:37:17 +0530

    Add files via upload

---
+++ 11-ternary-op.js
@@ -0,0 +1,16 @@
+/*let age = 21;
+age>=21;
+let message = age>=18 ? "You're an adult." : "You're a minor.";
+console.log(message); // Output: "You're an adult."*/
+
+/*let time = 16;
+let greeting = time < 12 ? "Good morning!" : time < 18 ? "Good afternoon!" : "Good evening!";
+console.log(greeting); // Output: "Good afternoon!"*/
+/*
+let isStudent = true;
+let message = isStudent ? "You get a student discount!" : "No discount available.";
+console.log(message); // Output: "You get a student discount!"*/
+
+let purchaseAmount = 150;
+let discount = purchaseAmount > 100 ? "10% discount applied." : "No discount available.";
+console.log(discount); // Output: "10% discount applied."
