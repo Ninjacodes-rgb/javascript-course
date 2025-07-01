e9f8a4e0af0a1550d8d1bd74cd90098160188775
Subham Das <subhamd3005@gmail.com>
2025-07-01 22:59:49 +0530

    Add files via upload

---
+++ 18-strict-equality-op.js
@@ -0,0 +1,14 @@
+/*
+= assignment operator
+== comparison operator
+=== strict equality operator
+!= non-strict inequality operator
+!== strict inequality operator
+*/
+const PI = 3.14;
+
+if(PI != 3.14) {
+  console.log("that is not PI");
+}else{
+  console.log("that is  PI");
+}
