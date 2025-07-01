07be05506603bd318cde8d4a3853858eb00c8c92
Subham Das <subhamd3005@gmail.com>
2025-07-01 22:59:26 +0530

    Add files via upload

---
+++ 17-logical-operators.js
@@ -0,0 +1,32 @@
+//AND = &&
+//OR = ||
+//NOT = !
+
+/*temp = -100;
+const temp = 20;
+if (temp>10 && temp < 30) {
+    console.log("It's a nice day");
+}
+else{
+    console.log("It's not a hot day");
+}*/
+/*
+temp = -100;
+const temp = 20;
+if (temp<=0 || temp > 30) {
+    console.log("It's a bad day");
+}
+else{
+    console.log("It's a good day");
+}
+*/
+
+const isSunny = true;
+    if(!isSunny){
+        console.log("It's a sunny day");
+    }
+    else{
+        console.log("It's not a sunny day");
+    }
+
+
